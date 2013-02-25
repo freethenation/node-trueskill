608baf710ae21a317bd41619e5f72f8d3fee1eeb
freethenation <rpklafter@yahoo.com>
2013-02-25 03:22:22 -0800

    1) Removed extra line in convert script

--- convert/convert.sh
+++ convert/convert.sh
@@ -3,4 +3,3 @@ set -x verbose #echo on
 coffee PyToCoffee
 cp trueskill.almost.coffee trueskill.coffee
 patch trueskill.coffee < trueskill.patch
-python test.py "[{\"skill\":[25.0, 8.33333333333],\"rank\":1},{\"skill\":[25.0, 8.33333333333],\"rank\":2},{\"skill\":[25.0, 8.33333333333],\"rank\":2},{\"skill\":[25.0, 8.33333333333],\"rank\":4}]"
