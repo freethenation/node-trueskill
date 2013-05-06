0b5b51c9a56ad4485182caa7ce6dbab5176743db
freethenation <rpklafter@yahoo.com>
2013-05-05 19:38:16 -0700

    recompiled

--- trueskill.js
+++ trueskill.js
@@ -12,7 +12,7 @@
     return new gaussian(player[0], player[1]);
   };
 
-  module.exports.chanceOfWinning = function(player1, player2) {
+  module.exports.ChanceOfWinning = function(player1, player2) {
     "The chance that trueskill assigns to player1 winning vs player2";    player1 = playerSkillToGuassian(player1);
     player2 = playerSkillToGuassian(player2);
     return 1.0 - player1.sub(player2).cdf(0.0);
